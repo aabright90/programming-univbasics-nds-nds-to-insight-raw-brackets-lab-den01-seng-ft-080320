c9c29f79d65c46e372cfb9548ddd38cbdd392c97
Austen Bright <pureaustenbright@gmail.com>
2020-03-20 02:17:48 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -2,10 +2,19 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 
 def directors_totals(nds)
+  directors = directors_database[0][:name]
+  gross = directors_database[0][:movies][0][:worldwide_gross]
   # Remember, it's always OK to pretty print what you get *in* to make sure
   # that you know what you're starting with!
-  #
-  #
+  directors_index = 0
+  total = 0
+  movies_index = 0
+  while directors_index < nds.length do
+    directors_index += 1
+    while movies_index < directors_database[directors_index][:movies].length do
+      binding.pry
+    end
+  end
   # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
   result = {
   }
