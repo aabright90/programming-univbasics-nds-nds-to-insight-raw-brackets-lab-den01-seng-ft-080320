c9fc1847a54401e793d4bca8d48274e74b107b64
Austen Bright <pureaustenbright@gmail.com>
2020-03-20 02:44:30 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -10,7 +10,7 @@ def directors_totals(nds)
   directors_index = 0
   while directors_index < nds.length do
     movies_index = 0
-    # total = 0
+    result[directors_database[directors_index][:name]] = 0
     while movies_index < directors_database[directors_index][:movies].length do
       if result[directors_database[directors_index][:name]]
       result[directors_database[directors_index][:name]] += directors_database[directors_index][:movies][movies_index][:worldwide_gross]
